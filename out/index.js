e01c329e49af3c8bc090c03bed005d0f4283d380
enjeytee <enjeytee@gmail.com>
2022-09-28 18:26:35 +0800

    final

--- out/index.js
+++ out/index.js
@@ -1,13 +1,12 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.screen = void 0;
-var button = document.querySelector(".button");
-var firstInput = document.querySelector("#first-input");
-var secondInput = document.querySelector("#second-input");
+const button = document.querySelector(".button");
+const firstInput = document.querySelector("#first-input");
+const secondInput = document.querySelector("#second-input");
 exports.screen = document.querySelector(".screen");
-var addNumbers = function (a, b) {
+const addNumbers = (a, b) => {
     exports.screen.innerHTML = (a + b).toString();
 };
-button.addEventListener("click", function () {
-    return addNumbers(parseFloat(firstInput.value), parseFloat(secondInput.value));
-});
+button.addEventListener("click", () => addNumbers(parseFloat(firstInput.value), parseFloat(secondInput.value)));
+//# sourceMappingURL=index.js.map
